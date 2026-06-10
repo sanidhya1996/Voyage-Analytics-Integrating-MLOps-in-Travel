28ad4f0a3bd4e2c4b615c674e314240124d17ba7
Sanidhya Shekhar <sanidhyashekhar1996@gmail.com>
2026-06-10 20:03:15 +0530

    testing

--- flight_price_api.py
+++ flight_price_api.py
@@ -10,7 +10,7 @@ import joblib
 import pandas as pd
 from flask import Flask, render_template_string, request, jsonify
 
-# we will do smoe changes here to check webhook is working or not !
+# we will do smoe changes here to check webhook is working or not !!!
 
 # ── Optional: pyngrok for public tunnel ──────────────────────────────────
 try:
